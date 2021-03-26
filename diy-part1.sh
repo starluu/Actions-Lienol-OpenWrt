f32fe1dd12fe6cb3fb9eac8536395f6e1b8eea5b
starluu <36126288+starluu@users.noreply.github.com>
2021-03-26 15:39:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
-
-# 添加xiaorouji大passwall源
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
