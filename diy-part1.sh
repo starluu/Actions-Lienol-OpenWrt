9ac5ad1aed9131966d0287386e304d2dce78c557
starluu <36126288+starluu@users.noreply.github.com>
2021-03-28 06:35:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
 
 # Add luci-app-bypass
+git clone --depth=1 https://github.com/jerrykuku/lua-maxmindd
 git clone --depth=1 https://github.com/garypang13/smartdns-le
 git clone --depth=1 https://github.com/garypang13/luci-app-bypass
