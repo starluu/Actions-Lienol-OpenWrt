d692ebf8550fd765092336e60c2f9530e903733e
starluu <36126288+starluu@users.noreply.github.com>
2021-03-28 11:28:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,6 @@
 #
 
 # Add a feed source
-sed -i '$a src-git smartdns-le https://github.com/garypang13/smartdns-le' feeds.conf.default
+git clone --depth=1 https://github.com/garypang13/smartdns-le
+#sed -i '$a src-git smartdns-le https://github.com/garypang13/smartdns-le' feeds.conf.default
 sed -i '$a src-git project-lede https://github.com/project-lede/openwrt-app' feeds.conf.default
