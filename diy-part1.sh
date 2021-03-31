a4035515eb813f7514670a460796b263385e3e8c
starluu <36126288+starluu@users.noreply.github.com>
2021-03-31 22:10:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,4 @@
 #
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
